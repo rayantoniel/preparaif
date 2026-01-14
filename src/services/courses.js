dd2db10e878103ff0fa8576b6a13baee5b655644
rayantoniel <antonielray5@gmail.com>
2026-01-14 14:06:48 -0300

    feat: Adiciona lista de cursos

---
+++ src/services/courses.js
@@ -0,0 +1,17 @@
+export const courses = [
+  {
+    id: 1,
+    campus: "IFAL - Campus Arapiraca",
+    name: "Bacharelado em Sistemas de Informação - Noturno",
+  },
+  {
+    id: 2,
+    campus: "IFAL - Campus Arapiraca",
+    name: "Tecnólogo em Logística - Noturno",
+  },
+  {
+    id: 3,
+    campus: "IFAL - Campus Maceió",
+    name: "Licenciatura em Ciências Biológicas - Noturno",
+  },
+];
